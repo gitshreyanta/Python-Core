2c38b18978cc5a0000b21126cc7e6beebed1a10e
Shreya@18 <shreyantasulebhavi1gmail.com>
2022-02-11 00:32:52 +0530

    [Shreya] Add: Python Program to take User Input Hello.

---
+++ Basic_Programs/Helloprogram.py
@@ -0,0 +1,12 @@
+"""
+Author: Shreyanta Sulebhavi
+Date: 2022-02-05 10:25:00
+Last Modified by: Shreyanta Sulebhavi
+Last Modified time: 2022-02-10 18:00:00
+Title : Hello Program
+"""
+
+#prints the length of username
+UserName = input("Please Enter the username :")
+print("Hello " + UserName +", How are you?")
+print("Length of UserName: " ,len(UserName))

9bc885d90527b44ac592598a1eae49fa3169c06b
Shreya@18 <shreyantasulebhavi1gmail.com>
2022-02-15 13:40:21 +0530

    [Shreya] Add: Python Program to Add Part-Time Emp Wages

--- Employee_Wages/EmpWage.py
+++ Employee_Wages/EmpWage.py
@@ -3,46 +3,49 @@ Author: Shreyanta Sulebhavi
 Date: 2022-02-14 10:25:00
 Last Modified by: Shreyanta Sulebhavi
 Last Modified time: 2022-02-14 21:00:00
-Title : UC2_Calculate Daily Employee Wage
+Title : UC3_Check add Part time Employee & Wage
 """
 import random
 
 def empWorkingHrs():
     """
     Description:
-        Function is used to Check Employee is Present or Absent.
+        Function is used to check Employee Present Absent & Half Day.
     Parameter:
-        Nothing  is used.
+        Nothing is used.
     Return:
         Returns Employee Hours
-    """  
-    attendance = random.randint(0,1)
+    """
+    attendance = random.randint(0,2)
     #print(attendance)
     if attendance == 0:
         print("Employee is Absent")
         emphrs = 0
     elif attendance == 1: 
         emphrs = 8
-        print("Employee is present")     
+        print("Employee is present")   
+    elif attendance == 2:
+        emphrs = 4
+        print("Employee is present half day")    
     return emphrs
 
-def empwages(emphrs):
+def empWages(emphrs):
+    global empRatePerHrs 
     """
     Description:
-        Function is used to Calculate Wage Per Day.
+        Function is used to Calculate Per day Wage.
     Parameter:
-        emphrs is used.
+        Employee Hours are used.
     Return:
-        Returns Employee Wages Per Day
+        Returns Employee Per Day wage
     """
-    global empRatePerHrs 
     empWagePerDay = emphrs * empRatePerHrs 
     return empWagePerDay 
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
     print("Welcome to Employee Wage Computation Program")
-    empRatePerHrs = 20           
+    empRatePerHrs = 20             
     emphrs = empWorkingHrs() 
-    totalEmpWage = empwages(emphrs)
+    totalEmpWage = empWages(emphrs)
     print("totalEmpWage per day is :",totalEmpWage)
 

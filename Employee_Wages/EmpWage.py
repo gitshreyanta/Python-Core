5f2601a599a13a57e9325b7af2ebbdddf18741ff
Shreya@18 <shreyantasulebhavi1gmail.com>
2022-02-15 13:31:54 +0530

    [Shreya] Add: Python Program to calculate daily Employee Wages

--- Employee_Wages/EmpWage.py
+++ Employee_Wages/EmpWage.py
@@ -1,28 +1,48 @@
 """
 Author: Shreyanta Sulebhavi
-Date: 2022-02-11 10:25:00
+Date: 2022-02-14 10:25:00
 Last Modified by: Shreyanta Sulebhavi
-Last Modified time: 2022-02-14 16:00:00
-Title : UC1_Check Employee is Present or Absent
+Last Modified time: 2022-02-14 21:00:00
+Title : UC2_Calculate Daily Employee Wage
 """
 import random
 
-def empAttendance():
+def empWorkingHrs():
     """
     Description:
-        Function is used Check Employee is Present or Absent.
+        Function is used to Check Employee is Present or Absent.
     Parameter:
-        Parameter are  not used.
+        Nothing  is used.
     Return:
-        Returns nothing
+        Returns Employee Hours
     """  
     attendance = random.randint(0,1)
-    print(attendance)
+    #print(attendance)
     if attendance == 0:
         print("Employee is Absent")
+        emphrs = 0
     elif attendance == 1: 
-        print("Employee is present")   
+        emphrs = 8
+        print("Employee is present")     
+    return emphrs
 
-if __name__ == "__main__": 
+def empwages(emphrs):
+    """
+    Description:
+        Function is used to Calculate Wage Per Day.
+    Parameter:
+        emphrs is used.
+    Return:
+        Returns Employee Wages Per Day
+    """
+    global empRatePerHrs 
+    empWagePerDay = emphrs * empRatePerHrs 
+    return empWagePerDay 
+
+if __name__ == "__main__":  
     print("Welcome to Employee Wage Computation Program")
-    empAttendance()  
+    empRatePerHrs = 20           
+    emphrs = empWorkingHrs() 
+    totalEmpWage = empwages(emphrs)
+    print("totalEmpWage per day is :",totalEmpWage)
+

a2e31afb1d69c71bfa82ac09ba251af31843c385
Shreya@18 <shreyantasulebhavi1gmail.com>
2022-02-15 13:46:32 +0530

    [Shreya] Add: Python Program using Switch case statement

--- Employee_Wages/EmpWage.py
+++ Employee_Wages/EmpWage.py
@@ -1,36 +1,70 @@
+from email.policy import default
 """
 Author: Shreyanta Sulebhavi
 Date: 2022-02-14 10:25:00
 Last Modified by: Shreyanta Sulebhavi
 Last Modified time: 2022-02-14 21:00:00
-Title : UC3_Check add Part time Employee & Wage
+Title : UC4_Solving Using Switch Case (Python Uses Dictionary concepts)
 """
 import random
 
-def empWorkingHrs():
+def empHrsForAbsent():
     """
     Description:
-        Function is used to check Employee Present Absent & Half Day.
+        Function is used to check Employee Absent.
     Parameter:
-        Nothing is used.
+        Paramater Nothing is used.
     Return:
-        Returns Employee Hours
+        Returns Employee Working Hours
     """
-    attendance = random.randint(0,2)
-    #print(attendance)
-    if attendance == 0:
-        print("Employee is Absent")
-        emphrs = 0
-    elif attendance == 1: 
-        emphrs = 8
-        print("Employee is present")   
-    elif attendance == 2:
-        emphrs = 4
-        print("Employee is present half day")    
+    emphrs = 0 
+    print("Employee is Absent")
     return emphrs
 
+def empHrsForPresent():
+    """
+    Description:
+        Function is used to check Employee Present.
+    Parameter:
+        Paramater Nothing is used.
+    Return:
+        Returns Employee Working Hours
+    """
+    emphrs = 8  
+    print("Employee is present")
+    return emphrs  
+def empHrsForHalfDay():
+    """
+    Description:
+        Function is used to check Employee Present half day.
+    Parameter:
+        Paramater Nothing is used.
+    Return:
+        Returns Employee Working Hours
+    """
+    emphrs = 4
+    print("Employee is present half day")
+    return emphrs 
+
+#Created a Dict for Employee Present , absent and Present halfday
+empAttendance = {
+    0: empHrsForAbsent,
+    1: empHrsForPresent,
+    2: empHrsForHalfDay
+}       
+
+def switch(attendance):
+    """
+    Description:
+        Function is used to switch to a particular function store in dict.
+    Parameter:
+        attendance are used.
+    Return:
+        Returns Employee Per Day wage
+    """    
+    return empAttendance.get(attendance, default)()
+
 def empWages(emphrs):
-    global empRatePerHrs 
     """
     Description:
         Function is used to Calculate Per day Wage.
@@ -39,13 +73,22 @@ def empWages(emphrs):
     Return:
         Returns Employee Per Day wage
     """
+    global empRatePerHrs 
     empWagePerDay = emphrs * empRatePerHrs 
-    return empWagePerDay 
+    return empWagePerDay
 
 if __name__ == "__main__":
     print("Welcome to Employee Wage Computation Program")
-    empRatePerHrs = 20             
-    emphrs = empWorkingHrs() 
-    totalEmpWage = empWages(emphrs)
-    print("totalEmpWage per day is :",totalEmpWage)
+    empRatePerHrs = 20 
+    attendance = random.randint(0,2)
+    empHrs = (switch(attendance))
+    totalEmpWage = empWages(empHrs)
+    print("TotalEmpWage per day is :",totalEmpWage)
+    
+                        
+
+
+   
+
+
 
